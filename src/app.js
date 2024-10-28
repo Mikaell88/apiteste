ba38a530ad1e43a1afcdfa5a6b7bb0d025ff4cc6
Mikaell88 <mikaelsby@outlook.com.br>
2024-10-27 21:44:50 -0300

    Primeiro commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+require('dotenv').config(); // Mantenha isso no topo do seu app.js
+
+
+const port = process.env.PORT || 3000;
+
+/* ---------- Configurações do Express ---------- */
+
+const app = express();
+
+app.use(express.json());
+app.use('/users', userRouter); // Importa as rotas que criamos no arquivo src/routes/user.js e as adiciona depois de /users/
+
+
+/* ---------- Criar conexão com o banco ---------- */
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Could not connect to MongoDB Atlas', err));
+
+
+/* ---------- Sobe nosso servidor de aplicação ---------- */
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/routes/user.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router(); // Utilizamos isso para criar o objeto de rotas que será exportado ao final
+const User = require('../models/user');
+
+// Create a new user
+router.post('/', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Get user by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+  });
+
+// Update a user
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a user
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
